48a5b3410efd5fbe62e377d9e85a53816e442416
Nicholas Finefter <nfinefter@gmail.com>
2021-04-21 09:58:25 -0700

    Added Ball and Sprite class

---
+++ BrickBreaker/Sprite.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrickBreaker
+{
+    class Sprite
+    {
+        public Vector2 Position { get; set; }
+        public Texture2D Texture { get; set; }
+        public Color Tint { get; set; }
+        public Vector2 Speed { get; set; }
+
+        public Point Size { get; set; }
+
+        public Rectangle HitBox
+        {
+            get
+            {
+                return new Rectangle(Position.ToPoint(), Size);
+            }
+        }
+
+        public Sprite(Vector2 position, Texture2D texture, Point size, Color tint, Vector2 speed)
+        {
+            Size = size;
+            Position = position;
+            Texture = texture;
+            Tint = tint;
+            Speed = speed;
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, HitBox, Tint);
+        }
+    }
+}
